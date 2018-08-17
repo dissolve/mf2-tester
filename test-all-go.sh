d14180b1f0c01beb250ce213501133700cad1d72
Ben Roberts <ben@thatmustbe.me>
2018-08-17 14:49:39 -0400

    better normalization

--- test-all-go.sh
+++ test-all-go.sh
@@ -2,5 +2,5 @@
 go build -o ./go/test-one-go ./scripts/test-one-go.go
 
 for f in vendor/mf2/tests/tests/microformats-*/*/*.html ; 
-    do ./go/test-one-go $f |jq -S . |sed 's/\[\]/{}/g' > `echo $f |sed s/vendor.mf2.tests.tests/go/| sed s/html/json/`;
+    do ./go/test-one-go $f |jq -S -f normalize.jq |sed 's/\[\]/{}/g' > `echo $f |sed s/vendor.mf2.tests.tests/go/| sed s/html/json/`;
 done;
