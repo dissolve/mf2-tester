0af3741edacd42fac77f7de6361bb49c4479e85e
Ben Roberts <ben@thatmustbe.me>
2018-08-23 14:14:32 -0400

    everyones a critic, @kartikprabhu

--- scripts/package.sh
+++ scripts/package.sh
@@ -44,13 +44,13 @@ for f in results/test-results/*/*/*.json ;
 
         NAME=`echo $TEST |sed 's/tests\///'|sed s/.txt//|sed 's/\//<br>/g'`;
 
-        echo '<tr>' >> results/index.html;
-        echo '<td><a href="'$TEST'">'$NAME'</a></td>' >> results/index.html;
+        echo '<tr>' >> results/table.html;
+        echo '<td><a href="'$TEST'">'$NAME'</a></td>' >> results/table.html;
 
         RESULT_MD5=`md5sum results/$RESULT |cut -d ' ' -f 1`;
         SHORT_MD5=`echo $RESULT_MD5 |cut -c 1-10`
         CLASS="pass";
-        echo "<td class='$CLASS'>Test Suite: <a href='$RESULT'>View</a><br><span class='md5' title='$RESULT_MD5'>$SHORT_MD5...</span></td>" >> results/index.html;
+        echo "<td class='$CLASS'>Test Suite: <a href='$RESULT'>View</a><br><span class='md5' title='$RESULT_MD5'>$SHORT_MD5...</span></td>" >> results/table.html;
         TOTAL=$[$TOTAL + 1]
 
         for lang in `ls languages`; do 
@@ -59,20 +59,20 @@ for f in results/test-results/*/*/*.json ;
             lang_RESULT_MD5=`md5sum results/$lang_RESULT |cut -d ' ' -f 1`;
             lang_SHORT_MD5=`echo $lang_RESULT_MD5 |cut -c 1-10`
             if [ "$RESULT_MD5" = "$lang_RESULT_MD5" ]; then
-                echo "<td class='pass'>Result: <a href='$lang_RESULT'>View</a> <br><span class='md5' title='$lang_RESULT_MD5'>$lang_SHORT_MD5...</span></td>" >> results/index.html;
+                echo "<td class='pass'>Result: <a href='$lang_RESULT'>View</a> <br><span class='md5' title='$lang_RESULT_MD5'>$lang_SHORT_MD5...</span></td>" >> results/table.html;
                 lang_total="count_$lang"
                 declare "count_$lang=$[${!lang_total} + 1]"
             else
                 diff results/$RESULT results/$lang_RESULT > results/$lang_RESULT.diff.txt
                 echo "<td class='fail'>Result: <a href='$lang_RESULT'>View</a> <br><span class='md5' title='$lang_RESULT_MD5'>$lang_SHORT_MD5...</span>
                 <div class='diff'><a href='$lang_RESULT.diff.txt'>Diff</a></div>
-                </td>" >> results/index.html;
+                </td>" >> results/table.html;
             fi
         done
 
 
 
-        echo '</tr>' >> results/index.html;
+        echo '</tr>' >> results/table.html;
 
 done;
 
@@ -81,12 +81,16 @@ echo "
 <td></td>
 <td></td> " >> results/index.html
 
+
 for lang in `ls languages`; do 
     lang_total="count_$lang"
     echo "<td>${!lang_total} of $TOTAL passed</td> " >> results/index.html
 done
 
-echo "</tr>
-</table>
+echo "</tr>" >> results/index.html
+
+cat results/table.html >> results/index.html
+
+echo "</table>
 </body>
 </html>" >> results/index.html
