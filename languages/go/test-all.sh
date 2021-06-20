4f5828eebb812fa78ec82bf8026b8e2c330c35b7
Will Norris <will@willnorris.com>
2021-06-19 20:21:25 -0700

    update go library to use go modules

--- languages/go/test-all.sh
+++ languages/go/test-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm languages/go/test-one 2>/dev/null
-go build -o ./languages/go/test-one ./languages/go/test-one.go
+go build -mod=mod -o ./languages/go/test-one ./languages/go/test-one.go
 
 for f in vendor/mf2/tests/tests/microformats-*/*/*.html ; 
     do ./languages/go/test-one $f |jq -S -f normalize.jq |sed 's/\[\]/{}/g' > `echo $f |sed 's/vendor.mf2.tests.tests/results\/go/' | sed s/html/json/`;
